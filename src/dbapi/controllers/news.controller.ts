82b02e97ad04cd5f6a20486b6447c6757d932a19
Lexa <amigos.73@mail.ru>
2022-04-02 22:38:12 +0700

    victory migrations

--- src/dbapi/controllers/news.controller.ts
+++ src/dbapi/controllers/news.controller.ts
@@ -5,23 +5,19 @@ import {
   Delete,
   Get,
   Post,
-  Put,
+  Render,
   Query,
   UseInterceptors,
   UploadedFile,
 } from '@nestjs/common';
 import { Response } from 'express';
-import { newsTemplate } from '../../views_data/news';
-import { DecrementId } from '../../utils/decrement-id.decorator';
-import { NewsDTO } from '../dto/news.dto';
 import { NewsService } from '../modules/news/news.service';
-import { newsDetail } from '../../views_data/news-detail';
-import { drawDocument } from '../../views_data/dcument';
-import { NewsIdDto } from '../dto/news-id.dto';
+import { NewsIdDto, NewsUpdateDto } from '../dto/news-id.dto';
 import { FileInterceptor } from '@nestjs/platform-express';
 import { diskStorage } from 'multer';
 import { HelperFileLoader } from '../../utils/HelperFileLoader';
 import { LoggingInterceptor } from '../modules/logger/logger.interceptor';
+import { NewsEntity } from '../database/entities/news.entity';
 
 const PATH_NEWS = 'news-static/';
 const helperFileLoader = new HelperFileLoader();
@@ -31,7 +27,7 @@ helperFileLoader.path = PATH_NEWS;
 export class NewsController {
   constructor(private readonly newsService: NewsService) {}
 
-  @Post('create')
+  @Post('create') // if exist id - update
   @UseInterceptors(
     LoggingInterceptor,
     FileInterceptor('cover', {
@@ -42,19 +38,19 @@ export class NewsController {
     }),
   )
   async createNews(
-    @Body() data: NewsDTO,
+    @Body() data: NewsUpdateDto,
     @UploadedFile() cover: Express.Multer.File,
     @Res() res: Response,
   ): Promise<void> {
+    const _newsEntity = { ...new NewsEntity(), ...data };
     let msg = '';
     let status = 200;
     try {
-      let coverPath = '';
       if (cover?.filename?.length > 0) {
-        coverPath = PATH_NEWS + cover.filename;
+        _newsEntity.cover = PATH_NEWS + cover.filename;
       }
 
-      await this.newsService.createNews({ ...data, cover: coverPath });
+      await this.newsService.create(_newsEntity);
       msg = 'Successfully created';
     } catch (e) {
       msg = (<any>e).detail;
@@ -63,35 +59,23 @@ export class NewsController {
     res.status(status).send(msg);
   }
 
-  @Post('update')
-  async updateNews(@Body() data: NewsDTO, @Res() res: Response): Promise<void> {
-    let msg = '';
-    let status = 200;
-    try {
-      msg = await this.newsService.updateNews(data);
-    } catch (e) {
-      msg = (<any>e).message;
-      status = 500;
-    }
-    res.status(status).send(msg);
-  }
-
   @Get('/')
-  async getNewsAll(): Promise<string> {
-    const news = await this.newsService.getNewsAll();
-    return drawDocument(newsTemplate(news));
+  async getNewsAll(@Res() res: Response): Promise<void> {
+    const news = await this.newsService.findAll();
+    res.render('news-list', { layout: 'index', news: news });
   }
 
   @Get('get-one')
   async getNewsOne(
-    @Query() @DecrementId(['id']) query: NewsIdDto,
-  ): Promise<string> {
-    const news = await this.newsService.getNews(query.id);
-    return drawDocument(newsDetail(news));
+    @Query() query: NewsIdDto,
+    @Res() res: Response,
+  ): Promise<void> {
+    const news = await this.newsService.findByIdWithAll(query.id);
+    res.render('news-one', { layout: 'index', news: news });
   }
 
   @Delete('delete')
-  async deleteNewsOne(@Body() body: NewsIdDto): Promise<NewsDTO[]> {
-    return this.newsService.deleteNews(body.id);
+  async deleteNews(@Body() body: NewsIdDto): Promise<NewsEntity | undefined> {
+    return this.newsService.remove(body.id);
   }
 }
--- src/dbapi/dto/news.dto.ts
+++ src/dbapi/dto/news.dto.ts
@@ -7,22 +7,11 @@ import {
   IsPositive,
   IsInt,
 } from 'class-validator';
-import { CommentDTO } from './comment.dto';
 
 export class NewsDTO {
-  // @IsInt()
-  // @IsPositive()
-  id!: number;
-
   @IsNotEmpty()
   @IsString()
-  name!: string;
-
-  @IsNotEmpty()
-  @IsDateString()
-  createdAt!: Date;
-
-  updatedAt!: Date;
+  title!: string;
 
   @ValidateIf((o) => o.description)
   @IsString()
@@ -31,11 +20,4 @@ export class NewsDTO {
   @ValidateIf((o) => o.cover)
   @IsString()
   cover!: string;
-
-  @IsString()
-  text!: string;
-
-  @ValidateIf((o) => o.comments)
-  @IsArray()
-  comments!: CommentDTO[];
 }
